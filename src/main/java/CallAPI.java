0574afb9ccf3f41d6b4ef962b547586405d555c7
kkolli <karishma.kolli@blackboard.com>
2017-01-17 13:13:00 -0500

    Changed Pom file

--- src/main/java/CallAPI.java
+++ src/main/java/CallAPI.java
@@ -6,14 +6,14 @@ public class CallAPI {
     public static void main(String[] args) {
 
        int fileCount;
-        //String fileLocation = "/Users/kkolli/desktop/results";
+       // String fileLocation = "/Users/kkolli/desktop/results";
         String fileLocation = args[0].toString();
         ReportGenerate report = new ReportGenerate();
         fileCount = report.generateJsonReport(fileLocation);
         FileCompare comp = new FileCompare();
         comp.getTestCases(fileCount);
         PractiTestRestCalls restCall = new PractiTestRestCalls();
-        //restCall.createTestSet();
+        restCall.createTestSet();
         restCall.moveTestsToTestSet();
         restCall.generateReportFromPractiTest();
         comp.compareTestSetAndUpdateResult();
